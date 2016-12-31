18f82f8a69e73f2ca035bf2ebc6d2d9b756c978b
angieMutava <mutavaangela@yahoo.com>
2016-12-30 20:01:18 -0800

    added updates

--- app/email.py
+++ app/email.py
@@ -13,29 +13,4 @@ def send_async_email(app, msg):
 	with app.app_context():
 		mail.send(msg)
 
-def send_mail(subject, sender, recipients, text_body, html_body):
-	msg = Message(subject, sender=sender, recipients=recipients)
-	msg.body = text_body
-	msg.html = html_body
-	thr = Thread(target=send_async_email, args=[app, msg])
-	thr.start()
-	return thr
-
-
-def notify_user(issues):
-	send_mail('%s' % issues.title,
-		       ADMINS[0],
-		       [issues.raised_by.email],
-		       render_template("send_mail.txt",
-		       					issues=issues),
-		       render_template("send_mail.html",
-		       					issues=issues))
-
-def assign_issue(issues):
-	send_mail('%s' % issues.title,
-		       ADMINS[0],
-		       [issues.assigned_to],
-		       render_template("send_mail_assign.txt",
-		       					issues=issues),
-		       render_template("send_mail_assign.html", issues=issues))
 
