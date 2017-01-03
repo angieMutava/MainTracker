ca7eef9e0c764229fabafc1d58ea80afa77c21b0
angieMutava <mutavaangela@yahoo.com>
2017-01-03 09:05:19 -0800

    added more updates

--- app/main/decorators.py
+++ app/main/decorators.py
@@ -1,14 +1,13 @@
 from functools import wraps
-from flask import redirect, url_for, flash
 from flask.ext.login import current_user
+from flask import abort
 
 def required_roles(role_id):
    def wrapper(f):
       @wraps(f)
       def wrapped(*args, **kwargs):
          if get_current_user_role() != role_id:
-            flash('sorry not allowed to access')
-            return redirect(url_for('home_page'))
+            abort(403)
          return f(*args, **kwargs)
       return wrapped
    return wrapper
